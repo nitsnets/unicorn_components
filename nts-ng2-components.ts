b80c50d36fe1ad02000f159f50f93a76493b49a6
berto <berto@q-interactiva.com>
2016-12-02 09:49:03 +0100

    Fix

--- nts-ng2-components.ts
+++ nts-ng2-components.ts
@@ -1,6 +1,6 @@
 export * from './src/nts.module';
 export * from './src/utils';
 export * from './src/models/option';
-export * from './src/modules/modal/modal.service';
-export * from './src/modules/modal/modal.interface';
-export * from './src/modules/modal/modal.module';
+// export * from './src/modules/modal/modal.service';
+// export * from './src/modules/modal/modal.interface';
+// export * from './src/modules/modal/modal.module';
