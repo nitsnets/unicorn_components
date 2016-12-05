a4fcbafc863b3c3b0da3314a89ea472919be488e
Alvaro <alvaro@nitsnets.com>
2016-12-05 11:53:24 +0100

    files added

---
+++ src/pipes/format-date.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({ name: 'formatDate' })
+export class FormatDatePipe implements PipeTransform {
+    transform(value: string): string {
+        if (!value) { return value; }
+        return moment(value).calendar();
+    }
+}
