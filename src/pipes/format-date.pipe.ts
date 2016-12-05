decb52862fd22e80f09392e1a2ed9b1ca4038bdf
Alvaro <alvaro@nitsnets.com>
2016-12-05 11:58:38 +0100

    - checkbox opacity fixed - formatDate pipe without warning

--- src/pipes/format-date.pipe.ts
+++ src/pipes/format-date.pipe.ts
@@ -3,8 +3,8 @@ import * as moment from 'moment';
 
 @Pipe({ name: 'formatDate' })
 export class FormatDatePipe implements PipeTransform {
-    transform(value: string): string {
-        if (!value) { return value; }
-        return moment(value).calendar();
-    }
+  transform(value: string): string {
+    if (!value) { return value; }
+    return moment(new Date(value)).calendar();
+  }
 }
