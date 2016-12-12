8bb720174908a722c3f5e45e6bd15b034ca7a9f6
Alvaro <alvaro@nitsnets.com>
2016-12-12 12:13:12 +0100

    - format Date improve - row and box fix

--- src/pipes/format-date.pipe.ts
+++ src/pipes/format-date.pipe.ts
@@ -3,8 +3,9 @@ import * as moment from 'moment';
 
 @Pipe({ name: 'formatDate' })
 export class FormatDatePipe implements PipeTransform {
-  transform(value: string): string {
+  transform(value: string, format: string = null): string {
     if (!value) { return value; }
-    return moment(new Date(value)).calendar();
+    if (!format) { return moment(new Date(value)).calendar(); }
+    return moment(new Date(value)).format(format);
   }
 }
