e107fdf172196a457eb19ed9b47a80f1b11e29d7
Paco Segovia García <paco@nitsnets.com>
2016-12-15 23:15:12 +0100

    Base + Paginator component (static)

---
+++ src/components/paginator/paginator.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'nts-paginator',
+    templateUrl: 'paginator.component.html',
+    styleUrls: ['paginator.component.scss'],
+})
+export class NtsPaginatorComponent {
+
+}
