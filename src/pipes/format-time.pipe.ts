97801c0f1bf0bf1f5356095587471f593d1e44b7
Alvaro <alvaro@nitsnets.com>
2017-01-11 13:30:22 +0100

    Format time fixed

--- src/pipes/format-time.pipe.ts
+++ src/pipes/format-time.pipe.ts
@@ -6,6 +6,6 @@ export class FormatTimePipe implements PipeTransform {
   transform(value: number, units: moment.DurationInputArg2): string {
     if (!value) { return ''; }
     let dur = moment.duration(value, units || 'seconds');
-    return `${dur.hours()}h ${dur.minutes()}min`;
+    return `${Math.floor(dur.asHours())}h ${dur.minutes()}min`;
   }
 }
