bef903b649ff0b42a002174f5a40b09b71b291eb
Alvaro <alvaro@nitsnets.com>
2017-02-13 18:23:30 +0100

    - Angular update to 2.4 - Component inheritance - Toggle component inherit from nts-checkbox

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,43 +1,43 @@
-import { NtsPopupComponent, NtsPopupTriggerDirective } from './popup/popup.component';
-import { NtsPopupContainerComponent } from './popup/container/container.component';
-
 import { NtsDropdownComponent, NtsDropdownTriggerDirective } from './dropdown/dropdown.component';
-import { NtsDropdownContainerComponent } from './dropdown/container/container.component';
-import { NtsDropdownItemComponent } from './dropdown/item/item.component';
+import { NtsPopupComponent, NtsPopupTriggerDirective } from './popup/popup.component';
 
-import { NtsFaceComponent } from './face/face.component';
-import { NtsBoxComponent } from './box/box.component';
-import { NtsRowComponent } from './row/row.component';
-import { NtsRowSpacerComponent } from './row-spacer/row-spacer.component';
+import { CodeEditorComponent } from './code/code.component';
 import { NtsAccordionComponent } from './accordion/accordion.component';
-import { NtsSearchHeaderComponent } from './search-header/search.component';
-import { NtsLayoutComponent } from './layout/layout.component';
 import { NtsAsideComponent } from './aside/aside.component';
-import { NtsSeparatorComponent } from './separator/separator.component';
-import { NtsInputComponent } from './input/input.component';
-import { NtsCheckboxComponent } from './checkbox/checkbox.component';
-import { NtsRadioComponent } from './radio/radio.component';
-import { NtsSelectComponent } from './select/select.component';
-import { NtsHeaderComponent } from './header/header.component';
-import { NtsBodyComponent } from './body/body.component';
-import { NtsButtonComponent } from './button/button.component';
-import { NtsIconComponent } from './icon/icon.component';
-import { NtsSimpleListComponent } from './simple-list/simple-list.component';
-import { NtsDatetimePickerComponent } from './datetime-picker/datetime-picker.component';
-import { NtsClockComponent } from './clock/clock.component';
-import { NtsCalendarComponent } from './calendar/calendar.component';
-import { NtsColorPickerComponent } from './color-picker/color-picker.component';
-import { NtsImagePickerComponent } from './image-picker/image-picker.component';
-import { NtsChipComponent } from './chip/chip.component';
-import { NtsProgressComponent } from './progress/progress.component';
 import { NtsBadgeComponent } from './badge/badge.component';
-import { NtsPaginatorComponent } from './paginator/paginator.component';
-import { NtsDaterangePickerComponent } from './daterange-picker/daterange-picker.component';
-import { NtsTabsComponent } from './tabs/tabs.component';
+import { NtsBodyComponent } from './body/body.component';
+import { NtsBoxComponent } from './box/box.component';
+import { NtsButtonComponent } from './button/button.component';
+import { NtsCalendarComponent } from './calendar/calendar.component';
 import { NtsChartComponent } from './chart/chart.component';
 import { NtsChartLegendComponent } from './chart-legend/chart-legend.component';
 import { NtsChartTooltipComponent } from './chart-tooltip/chart-tooltip.component';
-import { CodeEditorComponent } from './code/code.component';
+import { NtsCheckboxComponent } from './checkbox/checkbox.component';
+import { NtsChipComponent } from './chip/chip.component';
+import { NtsClockComponent } from './clock/clock.component';
+import { NtsColorPickerComponent } from './color-picker/color-picker.component';
+import { NtsDaterangePickerComponent } from './daterange-picker/daterange-picker.component';
+import { NtsDatetimePickerComponent } from './datetime-picker/datetime-picker.component';
+import { NtsDropdownContainerComponent } from './dropdown/container/container.component';
+import { NtsDropdownItemComponent } from './dropdown/item/item.component';
+import { NtsFaceComponent } from './face/face.component';
+import { NtsHeaderComponent } from './header/header.component';
+import { NtsIconComponent } from './icon/icon.component';
+import { NtsImagePickerComponent } from './image-picker/image-picker.component';
+import { NtsInputComponent } from './input/input.component';
+import { NtsLayoutComponent } from './layout/layout.component';
+import { NtsPaginatorComponent } from './paginator/paginator.component';
+import { NtsPopupContainerComponent } from './popup/container/container.component';
+import { NtsProgressComponent } from './progress/progress.component';
+import { NtsRadioComponent } from './radio/radio.component';
+import { NtsRowComponent } from './row/row.component';
+import { NtsRowSpacerComponent } from './row-spacer/row-spacer.component';
+import { NtsSearchHeaderComponent } from './search-header/search.component';
+import { NtsSelectComponent } from './select/select.component';
+import { NtsSeparatorComponent } from './separator/separator.component';
+import { NtsSimpleListComponent } from './simple-list/simple-list.component';
+import { NtsTabsComponent } from './tabs/tabs.component';
+import { NtsToggleComponent } from './toggle/toggle.component';
 
 export const NTS_COMPONENTS = [
     NtsPopupComponent, NtsPopupTriggerDirective, NtsPopupContainerComponent, NtsDropdownComponent, NtsDropdownTriggerDirective,
@@ -46,5 +46,6 @@ export const NTS_COMPONENTS = [
     NtsClockComponent, NtsIconComponent, NtsCalendarComponent, NtsAsideComponent, NtsLayoutComponent, NtsSearchHeaderComponent,
     NtsAccordionComponent, NtsColorPickerComponent, NtsImagePickerComponent, NtsSimpleListComponent, NtsBoxComponent, NtsRowComponent,
     NtsChipComponent, NtsProgressComponent, NtsFaceComponent, NtsBadgeComponent, NtsPaginatorComponent, NtsDaterangePickerComponent,
-    NtsTabsComponent, NtsChartComponent, NtsChartLegendComponent, NtsChartTooltipComponent, NtsRowSpacerComponent, CodeEditorComponent
+    NtsTabsComponent, NtsChartComponent, NtsChartLegendComponent, NtsChartTooltipComponent, NtsRowSpacerComponent, CodeEditorComponent,
+    NtsToggleComponent
 ];
---
+++ src/components/toggle/toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { NtsCheckboxComponent } from '../checkbox/checkbox.component';
+
+@Component({
+    selector: 'nts-toggle',
+    templateUrl: 'toggle.component.html',
+    styleUrls: ['toggle.component.scss'],
+})
+export class NtsToggleComponent extends NtsCheckboxComponent {
+    @Input() rounded = true;
+}
