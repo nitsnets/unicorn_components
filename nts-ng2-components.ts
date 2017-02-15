b56afb96a312a16381bb96dbbf254ff1dcc00ad9
Alvaro <alvaro@nitsnets.com>
2017-02-15 09:29:01 +0100

    modal and toast moved

--- nts-ng2-components.ts
+++ nts-ng2-components.ts
@@ -1,11 +1,12 @@
 export * from './src/nts.module';
 export * from './src/utils';
 export * from './src/models/option';
-export * from './src/modules/toast/toast.service';
-export * from './src/modules/toast/toast.module';
-export * from './src/modules/toast/toast.component';
 
-export * from './src/modules/modal/modal.service';
-export * from './src/modules/modal/modal.module';
-export * from './src/modules/modal/modal.component';
-export * from './src/modules/modal/modal.interface';
+export * from './src/components/utils/toast/toast.service';
+export * from './src/components/utils/toast/toast.module';
+export * from './src/components/utils/toast/toast.component';
+
+export * from './src/components/containers/modal/modal.service';
+export * from './src/components/containers/modal/modal.module';
+export * from './src/components/containers/modal/modal.component';
+export * from './src/components/containers/modal/modal.interface';
---
+++ src/components/base/form-input.component.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+export abstract class NtsFormInputComponent implements OnInit {
+
+    @Input() ntsModel;
+    @Output() ntsModelChange = new EventEmitter();
+    @Output() ntsFocus = new EventEmitter();
+    @Output() ntsBlur = new EventEmitter();
+
+    @Input() name: string = '';
+    @Input() label: string = '';
+    @Input() value: string = '';
+    @Input() required = false;
+    @Input() debounce = 0;
+
+    debounceTimer;
+    error
+
+    constructor() { }
+
+    ngOnInit() {
+        if (!this.ntsModel) {
+            this.ntsModel = this.value;
+        } else {
+            this.value = this.ntsModel;
+        }
+    }
+    onNgModelChange(ev) {
+        clearTimeout(this.debounceTimer);
+        this.debounceTimer = setTimeout(
+            _ => this.ntsModelChange.emit(ev), this.debounce || 0
+        );
+    }
+}
