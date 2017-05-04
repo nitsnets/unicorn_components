07886aa25dab5d9fb8e4017ebd17bdfdae526019
Alvaro <alvaro@nitsnets.com>
2017-05-04 12:49:08 +0200

    Sonar with A+++

--- src/components/utils/tooltip/tooltip.service.ts
+++ src/components/utils/tooltip/tooltip.service.ts
@@ -9,7 +9,8 @@ export class TooltipService extends DynamicContainerService {
     constructor(cmpFactoryResolver: ComponentFactoryResolver) { super(cmpFactoryResolver); }
 
     createTooltip(msg: string, options = {}, viewContainerRef: ViewContainerRef = this.defaultContainer) {
-        const tooltipRef: ComponentRef<NtsTooltipComponent> = this.attachComponent(NtsTooltipComponent, viewContainerRef);
+        const tooltipRef: ComponentRef<NtsTooltipComponent> =
+            this.attachComponent(NtsTooltipComponent, viewContainerRef);
 
         tooltipRef.instance.setMessage(msg);
         tooltipRef.instance.setOptions(options);
