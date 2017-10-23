d6090a104fdfa91d527408c3ed40f9b0643b6097
Alvaro <alvaro@nitsnets.com>
2017-10-23 17:30:29 +0200

    Test fixed to run in console

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['PhantomJS', 'Chrome'],
         singleRun: false
     });
 };
