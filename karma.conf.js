180077a7aad3058516c1641603e44abc55542123
Alvaro <alvaro@nitsnets.com>
2017-10-23 17:18:48 +0200

    Run tests with PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
         frameworks: ['jasmine', '@angular/cli'],
         plugins: [
             require('karma-jasmine'),
+            require('karma-phantomjs-launcher'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
@@ -29,7 +30,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS', 'Chrome'],
         singleRun: false
     });
 };
