437e1541d812846ff991d12d41736b9f4dc7c310
Andres-Sanchez <andres@nitsnets.com>
2017-11-30 11:34:55 +0100

    Add modifiers color classes to button component on root definition.

--- src/components/ui/button/button.component.ts
+++ src/components/ui/button/button.component.ts
@@ -10,5 +10,10 @@ import { UniButtonBaseComponent } from '../../base/button-base.component';
 export class UniButtonComponent extends UniButtonBaseComponent {
 
     @HostBinding('class.uni-button') componentClass = true;
+    @HostBinding('class.uni-button--default') get isDefault() { return this.color === 'default' }
+    @HostBinding('class.uni-button--primary') get isPrimary() { return this.color === 'primary' }
+    @HostBinding('class.uni-button--success') get isSuccess() { return this.color === 'success' }
+    @HostBinding('class.uni-button--error') get isError() { return this.color === 'error' }
+    @HostBinding('class.uni-button--warning') get isWarning() { return this.color === 'warning' }
     @Input() type: string;
 }
