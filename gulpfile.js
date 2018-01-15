33c0dc79e8f346c11da479273b809e2ad818ac26
Alvaro Yuste <alvaro@nitsnets.com>
2018-01-15 16:47:43 +0100

    Scripts move from Shell to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,67 @@
 const gulp = require('gulp');
+const remove = require('rimraf');
 const sass = require('node-sass');
+const replace = require('gulp-replace');
+const sequence = require('gulp-sequence');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const versioner = require('gulp-bump');
 const inlineTemplates = require('gulp-inline-ng2-template');
 const exec = require('child_process').exec;
+const version = require('./package.json').version;
+
+const temp = './tmp';
+const dist = './dist';
+const bundles = `${dist}/bundles`;
+const styles = `${dist}/styles`;
 
 /**
- * Inline external HTML and SCSS into Angular component files.
- * @see https://github.com/ludohenin/gulp-inline-ng2-template
+ * Main tasks
  */
-gulp.task('inline', () => {
-    return gulp.src(['./*.ts', './src/**/*.ts', '!./**/*.spec.ts'], { base: './' })
-        .pipe(inlineTemplates({
-            base: '/',
-            target: 'es6',
-            useRelativePaths: true,
-            styleProcessor: (path, ext, file, callback) => {
-                let compiledCss = sass.renderSync({
-                    file: path,
-                    outputStyle: 'compressed',
-                });
-                callback(null, compiledCss.css);
-            }
-        }))
-        .pipe(gulp.dest('./tmp/'));
+
+gulp.task('build', sequence('clean', 'prepare-build', 'compile', 'clean-temp', 'styles', 'bundle', 'minify'));
+gulp.task('deploy', sequence('build', 'prepare-deploy', 'publish'));
+
+/**
+ * Aux tasks
+ */
+
+gulp.task('clean', cb => remove(dist, cb));
+gulp.task('clean-temp', cb => remove(temp, cb));
+gulp.task('prepare-build', () => getSrcTs()
+    .pipe(inlineCmp())
+    .pipe(prepareImports())
+    .pipe(gulp.dest(temp))
+);
+gulp.task('compile', cb => compile(cb));
+gulp.task('bundle', cb => bundle(cb));
+gulp.task('minify', () => gulp.src(`${bundles}/unicorn.components.umd.js`)
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(bundles))
+);
+gulp.task('styles', () => gulp.src('src/styles/**/*.scss').pipe(gulp.dest(styles)));
+gulp.task('prepare-deploy', () => gulp.src('./package.dist.json')
+    .pipe(versioner({ version }))
+    .pipe(rename('package.json'))
+    .pipe(gulp.dest(dist))
+);
+gulp.task('publish', cb => publish(cb));
+/**
+ * Aux functions
+ */
+const getSrcTs = () => gulp.src(['./index.ts', './src/**/*.ts', '!./**/*.spec.ts'], { base: './' });
+const inlineCmp = () => inlineTemplates({
+    base: '/',
+    target: 'es6',
+    useRelativePaths: true,
+    styleProcessor: compileSass
 });
+const compileSass = (path, ext, file, cb) => {
+    let compiledCss = sass.renderSync({ file: path, outputStyle: 'compressed' });
+    cb(null, compiledCss.css);
+};
+const prepareImports = () => replace('import * as moment from', 'import moment from');
+const compile = cb => exec('./node_modules/.bin/ngc -p tsconfig.app.json', err => cb(err));
+const bundle = cb => exec('./node_modules/.bin/rollup -c', err => cb(err));
+const publish = cb => exec('npm publish', { cwd: dist }, err => cb(err));
--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f "dist/package.json" ]; then
-  echo "Project is not built."
-  npm run build
-fi
-
-echo "Starting deployment process."
-cd dist
-npm publish
