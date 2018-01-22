b07fbcd0260d5c5a7cf1827516b44fd1a234bd1c
Alvaro Yuste <alvaro@nitsnets.com>
2018-01-22 15:27:27 +0100

    Popup demo

---
+++ src/components/containers/popup/popup.component.demo.ts
@@ -0,0 +1,84 @@
+import { UniPopupComponent, UniPopupTriggerDirective } from './popup.component';
+import { UniPopupContainerComponent } from './container/container.component';
+import { UniIconComponent } from '../../utils/icon/icon.component';
+import { UniSpinnerComponent } from '../../utils/spinner/spinner.component';
+import { UniButtonComponent } from '../../ui/button/button.component';
+import { UniButtonPopupComponent } from '../../ui/button-popup/button-popup.component';
+import { UniButtonGroupComponent } from '../../ui/button-group/button-group.component';
+import { storiesOf } from '@storybook/angular';
+
+const moduleMetadata = {
+    declarations: [
+        UniPopupComponent,
+        UniPopupContainerComponent,
+        UniIconComponent,
+        UniSpinnerComponent,
+        UniButtonComponent,
+        UniButtonPopupComponent,
+        UniButtonGroupComponent,
+        UniPopupTriggerDirective
+    ]
+};
+const label = 'Click here!'
+const lorem = 'Est labore fugiat irure aliquip laborum minim ea eu in irure pariatur nulla.';
+
+storiesOf('Popup', module)
+    .add('Basic', () => ({
+        moduleMetadata, template: `
+        <uni-popup [closable]="false">
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Close button', () => ({
+        moduleMetadata, template: `
+        <uni-popup>
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Close if click inside', () => ({
+        moduleMetadata, template: `
+        <uni-popup [keepOpen]="false">
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Force position', () => ({
+        moduleMetadata, template: `
+        <uni-popup position="top-left">
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Force openness', () => ({
+        moduleMetadata, template: `
+        <uni-popup [opened]="true">
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Track openness', () => ({
+        moduleMetadata, template: `
+        <p>The popup is <strong>{{opened ? 'opened' : 'closed'}}</strong></p>
+        <uni-popup [(opened)]="opened">
+            <p uniPopupTrigger> ${label} </p>
+            <uni-popup-container> ${lorem} </uni-popup-container>
+        </uni-popup>
+    `}))
+    .add('Button triggered', () => ({
+        moduleMetadata, template: `
+        <uni-button-popup label="${label}" color="default" iconRight="add_circle">
+            ${lorem}
+        </uni-button-popup>
+    `}))
+    .add('Button group triggered', () => ({
+        moduleMetadata, template: `
+        <uni-button-group>
+            <uni-button color="default" label="Action button"></uni-button>
+            <uni-button-popup color="default" iconRight="info">
+                ${lorem}
+            </uni-button-popup>
+        </uni-button-group>
+    `}))
+    ;
